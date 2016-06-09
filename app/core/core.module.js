7be7b1d27e877971dee5a9d9ea5c254ca3c2bd79
mariocatch <mariocatch@gmail.com>
2016-06-09 15:39:49 -0400

    Enable header/footer/task components. Add styling.

--- app/core/core.module.js
+++ app/core/core.module.js
@@ -2,6 +2,21 @@
     'use strict';
 
     angular.module('hytit', [
-        'hytit.common'
-    ]);
+        'ui.router',
+
+        'hytit.common',
+        'hytit.header',
+        'hytit.task'
+    ])
+        .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+            $locationProvider.html5Mode(true);
+            $urlRouterProvider.otherwise('/list');
+
+            $stateProvider
+                .state('list', {
+                    url: '/',
+                    templateUrl: 'task/tasks.html',
+                    controller: 'TasksController'
+                });
+        });
 })();
