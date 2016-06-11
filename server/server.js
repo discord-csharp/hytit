ff8cee71c9f54cbfce9a16f197dc61cae3554e9f
@mariocatch <mariocatch@gmail.com>
2016-06-10 21:45:53 -0400

    Add mongo/mongoose and simple tasks get/post api

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
 var app = express();
 
 var host = process.env.APP_HOST || 'localhost';
@@ -9,6 +10,36 @@ var clientPath = path.join(__dirname, '../client', 'app', 'index.html');
 app.use(express.static('./client'));
 app.use(express.static('./client/app'));
 app.use(express.static('./'));
+app.use(bodyParser.urlencoded({ 'extended': 'true' }));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application.vnd.api+json' }));
+
+var Task = require('./task');
+
+app.get('/api/tasks', function (req, res) {
+    Task.find(function (err, tasks) {
+        if (err) {
+            res.send(err);
+        }
+
+        res.json(tasks);
+    });
+});
+
+app.post('/api/tasks', function (req, res) {
+    var promise = Task.create({
+        title: req.body.title,
+        description: req.body.description
+    }, function (err, task) {
+        if (err) {
+            res.send(err);
+        }
+
+        promise.then(function (task) {
+            res.json(task);
+        });
+    });
+});
 
 app.get('/', function (req, res) {
     res.sendFile(clientPath);
